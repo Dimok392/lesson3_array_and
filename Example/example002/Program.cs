835113467b90071109082578cc7dea8263ab7a1b
Пётр <dimok392@mail.ru>
2022-04-23 21:38:36 +0300

    Example002

---
+++ Example/example002/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("введите номер четверти");
+int a = int.Parse (Console.ReadLine());
+int x;
+int y;
+
+    if (a==1)
+    {
+        x = new Random().Next(0,100);
+        Console.WriteLine($"x={x}");
+        y = new Random().Next(0,100);
+        Console.WriteLine($"y={y}");
+    }
+    if(a==2)
+    {
+        x = new Random().Next(-100,0);
+        Console.WriteLine($"x={x}");
+        y = new Random().Next(0,100);
+        Console.WriteLine($"y={y}");
+    }
+    if (a==3)
+    {
+        x = new Random().Next(-100,0);
+        Console.WriteLine($"x={x}");
+        y = new Random().Next(-100,0);
+        Console.WriteLine($"y={y}");
+    }
+    if (a==4)
+    {
+        x = new Random().Next(0,100);
+        Console.WriteLine($"x={x}");
+        y = new Random().Next(-100,0);
+        Console.WriteLine($"y={y}");
+    }
+    else 
+    {  
+        Console.WriteLine("такой четверти нет");
+    }
