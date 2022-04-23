e31b5b7cf77e7366f43bead3bc1d9a21b9f3f3ed
Пётр <dimok392@mail.ru>
2022-04-23 20:39:50 +0300

    Example001

---
+++ Example/example001/Program.cs
@@ -0,0 +1,25 @@
+﻿int x = new Random().Next(-100,100);
+int y = new Random().Next(-100,100);
+Console.WriteLine($"x={x},y={y}");
+if (x==0 | y==0)
+{
+    Console.WriteLine("не принадлежит конкрктной четверти");
+
+}
+
+else if (x>0 & y>0)
+{
+    Console.WriteLine("точка принадлежит 1-ой четверти");
+}
+else if (x>0 & y<0)
+{
+    Console.WriteLine("точка принадлежит 2-ой четверти");
+}
+else if (x<0 & y<0)
+{
+    Console.WriteLine("точка принадлежит 3-ей четверти");
+}
+else if (x<0 & y>0)
+{
+    Console.WriteLine("точка принадлежит 4-ой четверти");
+}
