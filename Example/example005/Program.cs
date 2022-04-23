7658d9d17247a215de063c70cadaf325b86055d8
Пётр <dimok392@mail.ru>
2022-04-24 02:43:47 +0300

    Example005

---
+++ Example/example005/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 12821;
+int b = a / 1000;
+Console.WriteLine($"b={b}");
+int d = 10;
+int c = a % 100;
+Console.WriteLine($"c={c}");
+
+if (b % d == c / d & b / d == c % d)
+{
+    Console.WriteLine($"число {a} является полиндромом");
+}
+
+else
+{
+    Console.WriteLine($"число {a} не является полиндромом");
+}
